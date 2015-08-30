5245f97da85892b0ca39c3f342c1e10743d22e4e
Nathan Ockerman <ockerman.ns@gmail.com>
2015-08-30 00:49:04 -0700

    Notes update

--- models/phrases.js
+++ models/phrases.js
@@ -9,3 +9,9 @@ var PhrasesSchema = new Schema ({
 var Phrases = mongoose.model("Phrases", PhrasesSchema);
 
 module.exports = Phrases;
+
+// Is there a way to set up multiple sections of a data base? 
+// so as to make a "hard" "medium" & "easy" mode for the game. 
+// Also for keeping a list of wrong answers for the multiple 
+// choice section of the game (the form could add one wrong 
+// word for each word added to the db, thus growing both).
