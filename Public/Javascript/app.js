6a7ebd6175bf236b5389d1fdef378a7cb5bfad16
Nathan Ockerman <ockerman.ns@gmail.com>
2015-08-30 21:41:06 -0700

    Cleaned up code a bit.

--- Public/Javascript/app.js
+++ Public/Javascript/app.js
@@ -10,37 +10,37 @@
 
 
 $(document).ready(function(){
-	   //sanity check
-	console.log("Works!");
-    //render words in review tab
-	getWords();
+  	   //sanity check
+  	console.log("Works! We're up and running");
+      //render words in review tab
+  	getWords();
 
-  $("#user-add-phrase").on("submit", function (e){
-    e.preventDefault();
-        //slide away form and pop out alert
-    $("#contribute-form").slideToggle();
-    $("#thanks-alert").slideToggle();
-      //add new phrase
-    $.post("/phrases", $(this).serialize()) 
-      //clear form
-    $("#user-add-phrase")[0].reset();  
-    getWords();    //render new phrase to review tab
-    closeAlert("#thanks-alert", 2500); //closes alert
-  });
+    $("#user-add-phrase").on("submit", function (e){
+        e.preventDefault();
+            //slide away form and pop out alert
+        $("#contribute-form").slideToggle();
+        $("#thanks-alert").slideToggle();
+          //add new phrase
+        $.post("/phrases", $(this).serialize()) 
+          //clear form
+        $("#user-add-phrase")[0].reset();  
+        getWords();    //render new phrase to review tab
+        closeAlert("#thanks-alert", 2500); //closes alert
+    });
 
-  $("#contribute-button").on("click", function (e) {
-      e.preventDefault();
-      $("#contribute-form").slideToggle(); 
-  });
+    $("#contribute-button").on("click", function (e) {
+        e.preventDefault();
+        $("#contribute-form").slideToggle(); 
+    });
 
-  $("#word-add-cancel").on("click", function (e) {
-      e.preventDefault();
-      $("#contribute-form").slideToggle(); 
-  });
+    $("#word-add-cancel").on("click", function (e) {
+        e.preventDefault();
+        $("#contribute-form").slideToggle(); 
+    });
 
-  $("#game-start").on("click", function(){
-    getGameWords();
-  });
+    $("#game-start").on("click", function(){
+      getGameWords();
+    });
 
 });
  
@@ -53,41 +53,40 @@ $('#myTabs a').click(function (e) {
 });
 
 function closeAlert(selector, delay) {
-   var alert = $(selector).alert();
-   window.setTimeout(function() { alert.slideToggle() }, delay);
-   
+    var alert = $(selector).alert();
+    window.setTimeout(function() { alert.slideToggle() }, delay); 
 };
 
 function getWords() {
 	$.get("/phrases", function (res){
-		words = res.reverse();
-		console.log(words);
-		//render words
-		renderWords(words);
+		  words = res.reverse();
+	
+		  //render words
+		  renderWords(words);
 	});
 };
 
 function renderWords(words) {
-	var template = _.template($("#words-template").html());
-	console.log(template);
-	var wordListItem = words.map(function (word){
-		return template(word);
-	});
-	$('#phrase-list').html('');
-	$('#phrase-list').append(wordListItem);
+    var template = _.template($("#words-template").html());
+	
+  	var wordListItem = words.map(function (word){
+    	return template(word);
+    });
+  	$('#phrase-list').html('');
+  	$('#phrase-list').append(wordListItem);
 };
 
 function deletePhrase(context){
-  var phraseID = $(context).data()._id;
-  $.ajax({
-      url: "/phrases/" + phraseID,
-      type: "DELETE",
-      success: function(res){
-        $("#delete-alert").slideToggle();
-        closeAlert("#delete-alert", 2500);
-        getWords();
-      }
-  });
+    var phraseID = $(context).data()._id;
+    $.ajax({
+        url: "/phrases/" + phraseID,
+        type: "DELETE",
+        success: function(res){
+          $("#delete-alert").slideToggle();
+          closeAlert("#delete-alert", 2500);
+          getWords();
+        }
+    });
 };
 
 
@@ -103,41 +102,54 @@ function getAnswer(e){
     e.preventDefault();
   });
       // 1st if statment
-  if (turns < 10) {
-
-      console.log("< 10");
-
+  if (turns < 9) {
       var answerPicked = $('input[name="answerRadio"]:checked').val();
-      alert("Pick: " + answerPicked + " | Answer: " + correctAnswer);
         // 2nd (nested) if statment
       if (answerPicked === correctAnswer) { 
-          alert("You're right!");
+          $("#right-alert").slideToggle();
           score += 1;
           turns += 1;
+          closeAlert("#right-alert", 1500);
       } else if (answerPicked !== correctAnswer){
-          alert("Opps not quite. The answer was: " + answer);
+          $("#wrong-alert").slideToggle();
           turns += 1;
+          closeAlert("#wrong-alert", 1500);
       };
-
       getGameWords();
 
-  } else if (turns >= 10) {
-      alert("Finished. Your score is: " + score);
-  };
+  } else if (turns === 9) {
+    var answerPicked = $('input[name="answerRadio"]:checked').val();
+      alert("Pick: " + answerPicked + " | Answer: " + correctAnswer);
+        // 3rd (nested) if statment
+      if (answerPicked === correctAnswer) {           
+          $("#right-alert").slideToggle();      //same as above statment minus the getGameWords();
+          score += 1;
+          turns += 1;
+          closeAlert("#right-alert", 1500);
+      } else if (answerPicked !== correctAnswer){
+          $("#wrong-alert").slideToggle();
+          turns += 1;
+          closeAlert("#wrong-alert", 1500);
+      };
+      $('#question-placeholder').html('');
+      $("#gameOver-alert").slideToggle();
+      closeAlert("#gameOver-alert", 6500);
+      alert("Yay all done. Your score is: " + score);
+  }; /*else if (turns === 10) {
+      //var gameData = score;
+      //renderEndScreen();
+  };*/
 };
 
 
 function getGameWords(){
 	var phrases = words;
-	console.log(phrases);
 	randoQuestion(phrases);
 };
 
 function randoQuestion(phrases){
 	var randomGet = {};
 	randomGet = phrases[_.random(phrases.length-1)];
-	console.log(randomGet);
-
 	getQuestion(randomGet);
 };
 
@@ -149,17 +161,16 @@ function getQuestion(phrase){
     //set question and right answer
   clue = phrase.definition;
   answer = phrase.word; 
-  console.log("Clue: " + clue);
-  console.log("Answer: " + answer);
+
     // get wrong answers
   var wrong1 = wrongAnswer[_.random(wrongAnswer.length-1)];
   var wrong2 = wrongAnswer[_.random(wrongAnswer.length-1)];
   var wrong3 = wrongAnswer[_.random(wrongAnswer.length-1)];
-    console.log("1: " + wrong1 + ", 2: " + wrong2 + ", 3: " + wrong3);
+ 
     //get random number between 1 & 4
   var amount = 4;
   var randomNum = Math.floor(Math.random()*amount)+1
-      console.log(randomNum);
+
       //randomize and set answer list
   if (randomNum === 1) {
     aA = wrong1;
@@ -190,7 +201,6 @@ function getQuestion(phrase){
 };
 
 function renderAnswers(clue, a, b, c, d){
-	console.log("Clue: " + clue + ", A: " + a + ", B: " + b + ", C: " + c + ", D: " + d);
 	var questionInfo = { hint: "",
 						answerA: "",
 						answerB: "",
@@ -203,15 +213,30 @@ function renderAnswers(clue, a, b, c, d){
 	questionInfo.answerB = b;
 	questionInfo.answerC = c;
 	questionInfo.answerD = d;
-	console.log(questionInfo);
 
 	var questionTemplate = _.template($("#question-template").html());
-	console.log(questionTemplate);
 	var questionHTML = questionTemplate(questionInfo);
-	console.log(questionHTML);  // qClue, answerA, answerB, answerC, answerD
+	
 	$('#question-placeholder').html('');
 	$('#question-placeholder').append(questionHTML);
 };
+
+/*function renderEndScreen() {
+  //console.log(gameData);
+  var gameData = {points: ""};
+  scoreStr = score.toString();        // **** Could now get this block of code to work **** //
+  gameData.points = scoreStr;
+  console.log(gameData);
+
+  alert("Finished. Your score is: " + score);
+
+  var endScreenTemplate = _.template($("#game-end-template").html());
+  console.log(endScreenTemplate);
+  var endScreenHTML = endScreenTemplate(gameData);  // <<<<---------- where my issue is.
+  console.log(endScreenHTML);
+  //$('#game-end-placeholder').html('');
+  $("#game-end-placeholder").append(endScreenHTML);
+};*/
 
 ////////////////////////////////////////////
 
--- models/phrases.js
+++ models/phrases.js
@@ -9,31 +9,3 @@ var PhrasesSchema = new Schema ({
 var Phrases = mongoose.model("Phrases", PhrasesSchema);
 
 module.exports = Phrases;
-
-// Is there a way to set up multiple sections of a data base? 
-// so as to make a "hard" "medium" & "easy" mode for the game. 
-// Also for keeping a list of wrong answers for the multiple 
-// choice section of the game (the form could add one wrong 
-// word for each word added to the db, thus growing both).
-
-/*
-	*** For Morning ***
-
-	- Mongo db and server both work. Start them.
-	- All underscore templates work.
-	- Ask question above about parsing off sectinos of a DB.
-	- Still need to have form add entries to data base.
-	- Need to start on css and set up of the actual game. 
-		Although thus far I think I have accomplished all 
-		for the sprint reqs. 
-	- ** REMEMBER - Entry addition form should be contained 
-					in a 'contribute' button on the nav bar. 
-					Button will trigger toggle of a div that 
-					animates down from the nav bar and goes 
-					back up when submitted.
-
-	- You got this. You're almost done!! ***
-
-	- (Let Eden add a couple words to the database, 
-		she'll love it!)
-*/
